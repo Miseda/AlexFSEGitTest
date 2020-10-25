0860530efd02a22ddad3da5d1c3f5a853c0aa2ea
Alex Miseda <misedaalex@gmail.com>
2020-10-25 23:15:05 +0300

    First person commit

---
+++ person.java
@@ -0,0 +1,14 @@
+public class person{
+	String first;
+	String other;
+	String surname;
+	
+	public person (){
+		
+	}
+	
+	public thePerson ( String first, String surname){
+		this.first = first;
+		this.surname = surname;
+	}
+}
